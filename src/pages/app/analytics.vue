5696ff6d2fe6ea6923359e1506c42504e4362cfe
Baron <qbmrodrigo@gmail.com>
2023-05-17 05:46:01 +0800

    Pahabol ng data team

--- src/pages/app/analytics.vue
+++ src/pages/app/analytics.vue
@@ -8,7 +8,7 @@ definePage({
   <iframe
     title="PASADA FINAL - Duplicate of User Information"
     class="h-full w-full"
-    src="https://app.powerbi.com/view?r=eyJrIjoiYTgyYjA5ZDctMmMyZS00YWI4LTk3MzMtMjI0MDU0NzA0OWE1IiwidCI6ImM4ZmIwODBhLWM2NjYtNDViMy1hYWVmLTBjNDhkMWIzYTMxNCIsImMiOjEwfQ%3D%3D"
+    src="https://app.powerbi.com/view?r=eyJrIjoiNDZhZDg1MDEtZGM0Zi00ODY1LTk1OTEtNGQzMzBhMzIzMWJkIiwidCI6ImM4ZmIwODBhLWM2NjYtNDViMy1hYWVmLTBjNDhkMWIzYTMxNCIsImMiOjEwfQ%3D%3D"
     frameborder="0"
     allowFullScreen="true"
   />
