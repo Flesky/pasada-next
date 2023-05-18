8c98cd44315bf6b5ea5758c8543f46202b965a94
Baron <qbmrodrigo@gmail.com>
2023-05-19 07:46:19 +0800

    Data team URL

--- src/pages/app/analytics.vue
+++ src/pages/app/analytics.vue
@@ -8,7 +8,7 @@ definePage({
   <iframe
     title="PASADA FINAL - Duplicate of User Information"
     class="h-full w-full"
-    src="https://app.powerbi.com/view?r=eyJrIjoiNDZhZDg1MDEtZGM0Zi00ODY1LTk1OTEtNGQzMzBhMzIzMWJkIiwidCI6ImM4ZmIwODBhLWM2NjYtNDViMy1hYWVmLTBjNDhkMWIzYTMxNCIsImMiOjEwfQ%3D%3D"
+    src="https://app.powerbi.com/view?r=eyJrIjoiNWQ5OGVlYjUtN2I3Mi00YWFiLTkwMWItNWVkZDUzMDRiNjg3IiwidCI6ImM4ZmIwODBhLWM2NjYtNDViMy1hYWVmLTBjNDhkMWIzYTMxNCIsImMiOjEwfQ%3D%3D"
     frameborder="0"
     allowFullScreen="true"
   />
